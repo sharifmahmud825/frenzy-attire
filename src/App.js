ecb811cfe806d49d86931a8f9d44c9faa3d71adc
unknown <sharifmahmud825@gmail.com>
2022-05-23 21:15:01 +0600

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Home from './pages/Home/Home';
+
 function App() {
   return (
     <div className='App'>
-      <h1 className='text-3xl'>Hello world!</h1>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='container mx-auto'>
+        <div className='columns-2 bg-slate-500'>
+          <h1>logo</h1>
+        </div>
+        <div className='columns-6'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
